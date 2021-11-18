5f0abe1575bf1594da3ad17eaeca9507aa3e7423
bangzai458 <zmuttaqiena@gmail.com>
2021-11-18 17:48:57 +0700

    Update Sore

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Navbar from './Navbar/HomeNavbar'
-// import Login from './Product/LoginPage'
+import Login from './Product/LoginPage'
 import { Container } from 'react-bootstrap'
 import Banner from './Banner/HomeBanner'
 import ProductList from './Product/HomeProduct'
 import ProductAbout from './Product/HomeAbout'
 import ProductInfo from './Product/HomeInfo'
 // import Footer from './Footer/HomeFooter'
-import Footer2 from './Footer/Footer2'
+// import Footer2 from './Footer/Footer2'
 import '../Assets/style.css'
 
 
@@ -17,12 +17,12 @@ class Homepage extends React.Component {
             <Container fluid id="main-component">
                 <Navbar />
                 <Banner />
-                {/* <Login /> */}
+                <Login />
                 <ProductList /> 
                 <ProductAbout />
                 <ProductInfo />
                 {/* <Footer /> */}
-                <Footer2 />
+                {/* <Footer2 /> */}
             </Container>
         )
     }
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,19 +1,19 @@
 import React from 'react'
-// import Navbar from './Navbar/Navbar'
+import Navbar from './Navbar/HomeNavbar'
 // import Login from '../Container/LoginPage'
 import { Container } from 'react-bootstrap'
 import HomeBanner from './Banner/HomeBanner'
 // import ProductList from './Product/ProductList'
-import Footer from './Footer/HomeFooter'
+// import Footer from './Footer/HomeFooter'
 import '../Assets/style.css'
 
 class Main extends React.Component {
     render() {
         return (
             <Container fluid id="main-component">
-                {/* <Navbar /> */}
+                <Navbar />
                 <HomeBanner />
-                <Footer />               
+                {/* <Footer />                */}
             </Container>
         )
     }
